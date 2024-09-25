b3efa84103aedbc81ee5885f36f600b1b032fa86
SwayamprabhaGitHub <pswayamprabha@gmail.com>
2024-09-25 22:14:05 +0530

    Added login and logout function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,25 @@
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 2 });
+    dispatch(counterActions.increase(2));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
